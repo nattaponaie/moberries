b62ddad9311c5fcb28acb930ae36778868c17d65
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-12-12 20:13:20 +0700

    implement update total domain

--- src/api/v1.0/domains/payment.js
+++ src/api/v1.0/domains/payment.js
@@ -1,6 +1,6 @@
 import models from 'models';
 
-const create = ({ total, type = 0 }) => models.payments.create({
+const create = ({ total = 0, type = 0 }) => models.payments.create({
   type,
   total: parseFloat(total).toFixed(2),
 });
@@ -10,7 +10,16 @@ const updateOrderId = ({ orderId, paymentId }) => models.payments.update({ order
     id: paymentId,
   } });
 
+const updateTotal = async ({
+  total,
+  paymentId,
+}) => models.payments.update({ total: parseFloat(total).toFixed(2) },
+  { where: {
+    id: paymentId,
+  } });
+
 export default {
   create,
   updateOrderId,
+  updateTotal,
 };
