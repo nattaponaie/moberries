dde9ae409237ebeaa95fd2eb13a1ffc7ce0d26ac
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-12-12 19:39:57 +0700

    create json util

---
+++ src/utils/json.js
@@ -0,0 +1,19 @@
+import { reduce, camelCase, isArray, map, keys } from 'lodash/fp';
+
+const transformObjectToCamel = obj => reduce(
+  (camelCaseObject, key) => {
+    camelCaseObject[camelCase(key)] = obj[key];
+    return camelCaseObject;
+  },
+  {},
+  keys(obj),
+);
+
+const transformToCamelCaseKey = (obj) => {
+  if (isArray(obj)) {
+    return map(transformObjectToCamel, obj);
+  }
+  return transformObjectToCamel(obj);
+};
+
+export const transformSequelizeModel = sequelizeModel => transformToCamelCaseKey(JSON.parse(JSON.stringify(sequelizeModel)));
