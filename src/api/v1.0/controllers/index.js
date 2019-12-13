c474731d954079204dc57a06cb2b8f75043fe619
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-12-13 15:22:03 +0700

    put orderTransaction to index.js

--- src/api/v1.0/controllers/index.js
+++ src/api/v1.0/controllers/index.js
@@ -2,11 +2,13 @@ import BaseRoute from 'api/_baseRoute';
 
 import product from './product';
 import order from './order';
+import orderTransaction from './order-transaction';
 
 export default BaseRoute({
   version: '1.0',
   routes: [
     product,
     order,
+    orderTransaction,
   ],
 });
