9e66d53b8f57c9de2766f730bde334c6bc65cce6
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-12-14 15:06:56 +0700

    fix migrate file

--- src/database/migrations/20191210132205-add-order-id-column-to-payment.js
+++ src/database/migrations/20191210132205-add-order-id-column-to-payment.js
@@ -1,11 +1,12 @@
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.addColumn('payments', 'order_id', {
     type: Sequelize.INTEGER,
-    allowNull: false,
+    allowNull: true,
     references: {
       model: 'orders',
       key: 'id',
     },
+    field: 'order_id',
   }),
   down: queryInterface => queryInterface.removeColumn('payments', 'order_id'),
 };
