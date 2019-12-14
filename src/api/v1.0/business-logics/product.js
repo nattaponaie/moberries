90891a6c784ad4e3145a62099a8e5051d5e27994
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-12-14 12:25:31 +0700

    modify findProductByName

--- src/api/v1.0/business-logics/product.js
+++ src/api/v1.0/business-logics/product.js
@@ -3,9 +3,7 @@ import { lowerCase } from 'lodash';
 
 const findAll = async () => product.findAll();
 
-const findProductByName = async ({ name }) => {
-  return await product.findProductByName({ name: lowerCase(name) });
-};
+const findProductByName = async ({ name }) => await product.findProductByName({ name: lowerCase(name) });
 
 export default {
   findAll,
