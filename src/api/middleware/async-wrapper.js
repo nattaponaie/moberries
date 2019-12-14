274f65d80e0b0a690e9e4dff91b06ee80ef369d6
Nattapon Aiemlaor <nattapon.ai@easysunday.com>
2019-12-14 15:22:23 +0700

    fix async wrapper

--- src/api/middleware/async-wrapper.js
+++ src/api/middleware/async-wrapper.js
@@ -1,3 +1,13 @@
-export default fn => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
+import { get } from 'lodash';
+import { logError } from 'utils/logger';
+
+export default (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch((err) => {
+    logError(err);
+    const status = get(err, ['status'], 500);
+    const model = get(err, ['model']);
+    const message = get(err, ['message']);
+    res.status(status);
+    res.json({ status, model, message });
+  });
 };
